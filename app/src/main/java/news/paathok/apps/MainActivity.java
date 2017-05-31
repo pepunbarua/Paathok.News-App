f98d88ef44a4b307d96a071ff196fecb95c1ac1c
pepunbarua <pepunbarua@gmail.com>
2017-06-01 03:34:34 +0600

    navmenu / logo / setting menu

--- app/src/main/java/news/paathok/apps/MainActivity.java
+++ app/src/main/java/news/paathok/apps/MainActivity.java
@@ -106,17 +106,29 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.country) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.politics) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.courtsoflaw) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.economics) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.international) {
+
+        } else if (id == R.id.sports) {
+
+        } else if (id == R.id.entertainment) {
+
+        } else if (id == R.id.lifestyle) {
+
+        } else if (id == R.id.informationtechnology) {
+
+        } else if (id == R.id.chittagong) {
+
+        } else if (id == R.id.others) {
 
         }
 
